ac92e28d59d30ca1c9416c164fd3ab7665869ca4
osiastedian <osiastedian@gmail.com>
2015-08-21 13:31:33 +0800

    Added Function Definitions for UserDAO
    And renamed UsreService to UserService

--- src/main/java/com/crackers/informatronyx/dao/UserDAO.java
+++ src/main/java/com/crackers/informatronyx/dao/UserDAO.java
@@ -5,17 +5,75 @@
  */
 package com.crackers.informatronyx.dao;
 
+import com.crackers.informatronyx.config.AppConfig;
 import com.crackers.informatronyx.models.User;
-
+import com.mongodb.Mongo;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import org.springframework.data.mongodb.core.query.Query;
+import static org.springframework.data.mongodb.core.query.Query.query;
+import org.springframework.data.mongodb.core.query.Update;
 /**
  *
  * @author Ted Ian Osias
  */
 public class UserDAO {
     
-    public static User getUser(){ return null;}
-    public static boolean addUser(User user){return false;}
-    public static boolean editUser(User user){return false;}
-    public static boolean deleteUser(User user){return false;}
-    public static boolean exists(String username){return false;}
+    
+    public static User getUser(User user) throws UnknownHostException{ 
+        MongoOperations mongoOps = new MongoTemplate(new Mongo(AppConfig.mongodb_host, AppConfig.mongodb_port),"database");
+        User p = null;
+        p = mongoOps.findOne(query(where("id").is(user.getId()).orOperator(where("username").is(user.getUsername()))), User.class);
+        return p;
+    }
+    public static boolean addUser(User user) throws UnknownHostException{
+        MongoOperations mongoOps = new MongoTemplate(new Mongo(AppConfig.mongodb_host, AppConfig.mongodb_port),"database");
+        boolean ok = false;
+        mongoOps.insert(user);
+        ok = true;
+        return ok;
+    }
+    public static boolean editUser(User user) throws UnknownHostException{
+        MongoOperations mongoOps = new MongoTemplate(new Mongo(AppConfig.mongodb_host, AppConfig.mongodb_port),"database");
+        boolean ok = false;
+        Query query = new Query();
+        query.addCriteria(where("id").is(user.getId()));
+        Update update = new Update();
+        update.addToSet("username", user.getUsername());
+        update.addToSet("password", user.getPassword());
+        mongoOps.updateFirst(query,update,User.class);
+        ok = true;
+        return ok;
+    }
+    public static boolean deleteUser(User user) throws UnknownHostException{
+        MongoOperations mongoOps = new MongoTemplate(new Mongo(AppConfig.mongodb_host, AppConfig.mongodb_port),"database");
+        boolean ok = false;
+        mongoOps.remove(user);
+        ok = true;
+        return ok;
+    }
+    public static void main(String []args){
+        try {
+            User user = new User("osiastedian","osias.tedian");
+           // System.out.println(UserDAO.addUser(user));
+            //user = UserDAO.getUser(user);
+            //UserDAO.deleteUser(user);
+            // EXIST//
+            System.out.println("Chuya"+UserDAO.getUser(user));//UserDAO.exists("osiastedian"));
+        } catch (UnknownHostException ex) {
+            
+        }
+    }
+    public static boolean exists(String username) throws UnknownHostException{
+        MongoOperations mongoOps = new MongoTemplate(new Mongo(AppConfig.mongodb_host, AppConfig.mongodb_port),"database");
+        boolean ok = false;
+        Query query = new Query();
+        query.addCriteria(where("username").is(username));
+        ok = mongoOps.exists(query, User.class);
+        return ok;
+    }
 }
